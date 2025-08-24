aa57f525535368575fbb1015ce79546f6f51fb94
York <supbut@qq.com>
2025-08-25 09:09:42 +1200

    db table selection

---
+++ PineDroid/src/main/java/com/pine/pinedroid/activity/sql/RunSqlScreenStatus.kt
@@ -0,0 +1,18 @@
+package com.pine.pinedroid.activity.sql
+
+
+
+
+import com.pine.pinedroid.db.bean.DatabaseInfo
+import com.pine.pinedroid.db.bean.TableInfo
+
+
+data class RunSqlScreenStatus(
+    val dbName: String = "Default Database",
+    val tableName: String = "Default Table",
+
+){
+
+
+}
+
--- PineDroid/src/main/java/com/pine/pinedroid/activity/table_selection/TableSelection.kt
+++ PineDroid/src/main/java/com/pine/pinedroid/activity/table_selection/TableSelection.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -58,7 +59,7 @@ fun TableSelection(
 
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
+            TopAppBar(
                 title = {
                     Text(
                         text = viewState.dbName,
@@ -76,10 +77,6 @@ fun TableSelection(
                         }
                     }
                 },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
             )
         }
     ) { innerPadding ->
