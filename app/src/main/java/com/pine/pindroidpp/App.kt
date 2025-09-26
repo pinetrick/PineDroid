8d3e73f62061b12f8c42bc05136ccd81563440c7
pine <admin@intbr.com>
2025-09-26 20:36:38 +1200

    profile

--- app/src/main/java/com/pine/pindroidpp/App.kt
+++ app/src/main/java/com/pine/pindroidpp/App.kt
@@ -2,6 +2,7 @@ package com.pine.pindroidpp
 
 import android.app.Application
 import com.pine.pinedroid.PineConfig
+import com.pine.pinedroid.debug.window.PineDebugWindow
 import com.pine.pinedroid.language.SupportedLanguages.Companion.SUPPORTED_LANGUAGES
 import com.pine.pinedroid.language.language_switch_screen.LanguageSwitchScreenVM
 
@@ -9,7 +10,7 @@ class App: Application()
 {
     override fun onCreate() {
         super.onCreate()
-        PineConfig.setIsDebugWindowAlwaysOn(true)
+        PineDebugWindow.setIsDebugWindowAlwaysOn(true)
 
         LanguageSwitchScreenVM.SUPPORTED_LANGUAGES = SUPPORTED_LANGUAGES.filter {
             it.code in listOf("system", "en", "zh", "hi", "es", "de", "fr", "ja", "ko")
