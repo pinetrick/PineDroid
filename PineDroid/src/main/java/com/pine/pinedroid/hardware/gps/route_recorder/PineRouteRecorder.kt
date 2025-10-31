f2186e4956c3715e79ce984095819d62634e1304
pine <admin@intbr.com>
2025-10-31 21:02:25 +1300

    location

--- PineDroid/src/main/java/com/pine/pinedroid/hardware/gps/route_recorder/PineRouteRecorder.kt
+++ PineDroid/src/main/java/com/pine/pinedroid/hardware/gps/route_recorder/PineRouteRecorder.kt
@@ -3,25 +3,26 @@ package com.pine.pinedroid.hardware.gps.route_recorder
 import com.pine.pinedroid.db.table
 import com.pine.pinedroid.hardware.gps.PineLatLng
 import com.pine.pinedroid.hardware.gps.PineLocationForegroundService
-import com.pine.pinedroid.jetpack.viewmodel.NavEvent
-import com.pine.pinedroid.utils.log.logi
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 
 open class PineRouteRecorder internal constructor() {
 
     //必须有强引用，否则会被gc
     private val locationCallback: (PineLatLng) -> Unit = ::onLocationChanged
+    private var lastRecord: PineLatLng? = null
 
     init {
         table<PineRecorderLatLngBean>().createTable()
         PineLocationForegroundService.subscribe(locationCallback)
     }
 
+    fun getLastLocation() : PineLatLng? {
+        return lastRecord
+    }
+
     private fun onLocationChanged(location: PineLatLng) {
+        lastRecord = location
         location.toRecorderLatLngBean().save()
 
         _navEvents.value = location
