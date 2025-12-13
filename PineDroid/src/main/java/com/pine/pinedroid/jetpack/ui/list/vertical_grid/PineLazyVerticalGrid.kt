fd42558e866f898cae3a4334f25a0b4029a783dc
pine <admin@intbr.com>
2025-12-13 18:00:56 +1300

    位置

--- PineDroid/src/main/java/com/pine/pinedroid/jetpack/ui/list/vertical_grid/PineLazyVerticalGrid.kt
+++ PineDroid/src/main/java/com/pine/pinedroid/jetpack/ui/list/vertical_grid/PineLazyVerticalGrid.kt
@@ -1,7 +1,9 @@
 package com.pine.pinedroid.jetpack.ui.list.vertical_grid
 
 
+import android.content.res.Configuration
 import androidx.compose.foundation.OverscrollEffect
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Arrangement
@@ -14,12 +16,19 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.rememberOverscrollEffect
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.pine.pinedroid.jetpack.ui.list.shopping.PineShoppingItemBean
+import com.pine.pinedroid.jetpack.ui.list.shopping.PineShoppingItemBean.Companion.ShoppingItemBeanDemo
+import com.pine.pinedroid.jetpack.ui.list.shopping.PineShoppingListItemVertical
+import com.pine.pinedroid.jetpack.ui.search.no_record.PineSearchNoResult
 
 /*
 * ScoreBar must make sure every element has same high */
@@ -57,4 +66,33 @@ fun PineLazyVerticalGrid(
         )
     }
 
+}
+
+
+@Preview(
+    showBackground = true,
+    widthDp = 360,
+    heightDp = 800,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Composable
+private fun PreviewGrid() {
+
+
+    PineLazyVerticalGrid(
+        contentPadding = PaddingValues(3.dp),
+        horizontalArrangement = Arrangement.spacedBy(3.dp),
+        verticalArrangement = Arrangement.spacedBy(3.dp),
+        columns = GridCells.Adaptive(150.dp),
+        modifier = Modifier.background(Color.Transparent.copy(alpha = 0.1f))
+    ) {
+
+        items(ShoppingItemBeanDemo) { destination ->
+            PineShoppingListItemVertical(
+                shoppingItemBean = destination,
+                onItemClick = {  },
+                modifier = Modifier.fillMaxHeight(2f)
+            )
+        }
+    }
 }
