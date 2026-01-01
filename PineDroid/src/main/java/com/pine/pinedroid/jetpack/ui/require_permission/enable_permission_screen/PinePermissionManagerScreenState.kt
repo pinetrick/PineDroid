2abfa9d129761ba6b9daa55fa5797f2fa04f3a96
pine <admin@intbr.com>
2026-01-01 21:13:05 +1300

    导航 反馈

--- PineDroid/src/main/java/com/pine/pinedroid/jetpack/ui/require_permission/enable_permission_screen/PinePermissionManagerScreenState.kt
+++ PineDroid/src/main/java/com/pine/pinedroid/jetpack/ui/require_permission/enable_permission_screen/PinePermissionManagerScreenState.kt
@@ -1,13 +1,15 @@
 package com.pine.pinedroid.jetpack.ui.require_permission.enable_permission_screen
 
 import com.pine.pinedroid.hardware.permission.PineOnePermission
+import com.pine.pinedroid.utils.shrinker_keep.Keep
 
 
-
+@Keep
 data class PinePermissionManagerScreenState(
     var permissionList: List<PermissionItem> = emptyList(),
 )
 
+@Keep
 data class PermissionItem(
     val granted: Boolean,
     val permission: PineOnePermission
